6339e365771f1eb5b2474c21eb7f833ca5adeee8
Mert <mert.dogan@me.com>
2025-05-19 13:45:25 +0200

    error fixes :)

--- backend/main.py
+++ backend/main.py
@@ -3,44 +3,36 @@ from pydantic import BaseModel
 from typing import Dict, Set
 import uuid
 import random
-from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Oder z. B. ["http://localhost:3000"]
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = FastAPI()
 
 # 1 = Runnging, 0 = Waiting
 status = 0
 
-# Spieler-ID → Song-ID
-submitted_songs: Dict[uuid.UUID, str] = {}
+submitted_songs = []
 cur_songs = []
 
 # Spieler-ID (die sich als ready gemeldet haben)
 ready_players: Set[uuid.UUID] = set()
-players = set()
+players = []
 
 # Anzahl benötigter Spieler
 required_players = 2
-required_players = 1
 
 class Cur_Song():
-    def __init__(self, id):
+    def __init__(self, id, submitter):
+        self.submitter = submitter
         self.id = id
 
     votes: Set[uuid.UUID] = set()
     id: str
+    submitter: uuid.UUID
 
 class Song(BaseModel):
     song: str
 
 @app.post("/submit")
-async def submit(song: Song):
+async def post_submit(song: Song):
     if status == 1:
         raise HTTPException(status_code=403, detail="Game already started")
 
@@ -48,7 +40,7 @@ async def submit(song: Song):
     player_id = uuid.uuid4()
 
     # Song speichern
-    submitted_songs[player_id] = song.song
+    submitted_songs.append(Cur_Song(song.song, player_id))
     players.append(player_id)
 
     print(f"Spieler {player_id} hat Song {song.song} eingereicht")
@@ -56,15 +48,15 @@ async def submit(song: Song):
     return { "player_id": player_id }
 
 @app.get("/getcur")
-async def getcur():
+async def get_cur():
     return cur_songs
 
 @app.get("/status")
-async def status():
+async def get_status():
     return { "status": status }
 
 @app.get("/ready")
-async def ready(id: str):
+async def get_ready(id: str):
     if status == 1:
         raise HTTPException(status_code=403, detail="Game already started")
 
@@ -79,11 +71,12 @@ async def ready(id: str):
     return {"status": "waiting" }
 
 def start():
+    global status
     assert status == 0
 
     status = 1
     random.shuffle(submitted_songs)
-    cur_songs[Song(submitted_songs[0]), Song(submitted_songs[1])]
+    cur_songs = [submitted_songs[0], submitted_songs[1]]
 
 def vote(player: uuid.UUID, song: str):
     for cur_song in cur_songs:
